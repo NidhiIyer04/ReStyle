e24f3d14afbcb9d450df6f6e31971ea7be238895
NidhiIyer04 <nidhiiyer240604@gmail.com>
2024-07-30 23:22:44 +0530

    Improved UI

--- Interface/model.py
+++ Interface/model.py
@@ -8,13 +8,10 @@ import colorsys
 import streamlit as st
 import pandas as pd
 import subprocess
-
-# MediaPipe setup
+import time
 mp_drawing = mp.solutions.drawing_utils
 mp_selfie_segmentation = mp.solutions.selfie_segmentation
 
-
-# Function to apply selfie segmentation
 def apply_selfie_segmentation(image, model_selection):
     with mp_selfie_segmentation.SelfieSegmentation(model_selection=model_selection) as selfie_segmentation:
         results = selfie_segmentation.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
@@ -24,8 +21,6 @@ def apply_selfie_segmentation(image, model_selection):
         output_image = np.where(condition, image, bg_image)
         return output_image
 
-
-# Function to detect faces in an image
 def detect_faces(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -33,7 +28,6 @@ def detect_faces(image):
     return faces
 
 
-# Function to crop the image to the first detected face
 def crop_to_face(image, faces):
     if len(faces) == 0:
         return None
@@ -41,8 +35,6 @@ def crop_to_face(image, faces):
     cropped_image = image[y:y + h, x:x + w]
     return cropped_image
 
-
-# Function to detect dominant colors in an image and optionally lip color
 def detect_colors(image, num_colors=20, detect_lip_color=False):
     img = np.array(image)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Ensure image is in RGB format for KMeans
@@ -51,30 +43,24 @@ def detect_colors(image, num_colors=20, detect_lip_color=False):
     kmeans.fit(img)
     colors = kmeans.cluster_centers_
 
-    # Filter out gray colors
     filtered_colors = []
     for color in colors:
         r, g, b = color
         if not (r == g == b or abs(r - g) < 15 and abs(r - b) < 15 and abs(g - b) < 15):
             filtered_colors.append(tuple(map(int, color)))
 
-    # Optionally detect lip color (you may need to fine-tune the ROI for lips)
     if detect_lip_color:
-        # Define ROI for lips
-        # Example coordinates; adjust based on your specific use case
+
         lip_roi = image[150:250, 100:300]  # Example ROI coordinates
 
-        # Convert ROI to RGB
         lip_roi_rgb = cv2.cvtColor(lip_roi, cv2.COLOR_BGR2RGB)
         lip_roi_rgb = lip_roi_rgb.reshape((lip_roi_rgb.shape[0] * lip_roi_rgb.shape[1], 3))
 
-        # Apply KMeans clustering to detect dominant colors in lip ROI
         kmeans_lip = KMeans(n_clusters=1)
         kmeans_lip.fit(lip_roi_rgb)
         lip_color = tuple(map(int, kmeans_lip.cluster_centers_[0]))
         filtered_colors.append(lip_color)
 
-    # Ensure exactly 20 colors are returned
     if len(filtered_colors) < 20:
         num_additional_colors = 20 - len(filtered_colors)
         additional_colors = np.random.randint(0, 256, size=(num_additional_colors, 3))
@@ -83,23 +69,18 @@ def detect_colors(image, num_colors=20, detect_lip_color=False):
     return filtered_colors[:20]
 
 
-# Function to convert RGB to HSL and return hue, saturation, and lightness
 def rgb_to_hsl(r, g, b):
     h, l, s = colorsys.rgb_to_hls(r / 255, g / 255, b / 255)
-    h = h * 360  # Convert hue to degrees
-    s = s * 100  # Convert saturation to percentage
-    l = l * 100  # Convert lightness to percentage
+    h = h * 360  
+    s = s * 100  
+    l = l * 100  
     return h, s, l
 
-
-# Function to determine temperature from hues
 def determine_temperature(hues):
     warm_hues = sum(1 for h in hues if 0 <= h <= 60 or 300 <= h <= 360)
     cool_hues = len(hues) - warm_hues
     return "Warm" if warm_hues >= cool_hues else "Cool"
 
-
-# Function to determine overall depth
 def determine_depth(lightness_values):
     average_lightness = sum(lightness_values) / len(lightness_values)
     if average_lightness < 20:
@@ -113,8 +94,6 @@ def determine_depth(lightness_values):
     else:
         return "Very Light"
 
-
-# Function to determine overall chroma
 def determine_chroma(saturation_values):
     average_saturation = sum(saturation_values) / len(saturation_values)
     if average_saturation < 20:
@@ -124,7 +103,6 @@ def determine_chroma(saturation_values):
     else:
         return "High"
 
-# Function to map colors to 12-season color theory
 def map_to_season(hues, lightness_values, saturation_values):
     seasons = {
         'Bright Spring': 0,
@@ -141,8 +119,7 @@ def map_to_season(hues, lightness_values, saturation_values):
         'Bright Winter': 0
     }
     for h, l, s in zip(hues, lightness_values, saturation_values):
-        # Determine the season based on hue, lightness, and saturation
-        if 0 <= h < 45 or 330 <= h <= 360:  # Warm hues
+        if 0 <= h < 45 or 330 <= h <= 360:  
             if s > 50 and l > 50:
                 seasons['Bright Spring'] += 1
             elif s > 50 and l <= 50:
@@ -170,11 +147,10 @@ def map_to_season(hues, lightness_values, saturation_values):
                 seasons['True Winter'] += 1
             elif s <= 50 and l > 50:
                 seasons['Bright Winter'] += 1
-    # Determine the predominant season
+
     predominant_season = max(seasons, key=seasons.get)
     return predominant_season
 
-# Function to recommend colors based on the season
 def recommend_colors(season):
     color_map = {
         'Bright Spring': 'red-yellow-blue',
@@ -193,8 +169,7 @@ def recommend_colors(season):
     colors = color_map.get(season, 'black')
     color_query = '+'.join(colors.split('-'))
     url = f"https://www.myntra.com/{colors}?rawQuery={color_query}"
-    
-    # Define some example color palettes
+
     color_ranges = {
         'Bright Spring': [(255, 0, 0), (255, 255, 0), (0, 0, 255)],
         'True Spring': [(255, 255, 0), (0, 255, 0), (0, 0, 255)],
@@ -212,102 +187,85 @@ def recommend_colors(season):
     palette = color_ranges.get(season, [])
     
     return [url, palette]
-
-
-# Function to display the season and recommended colors
-def display_season_and_colors(season, colors):
-    st.write(f"Congratulations!!!! You are a **{season}** season.")
-    st.write("Here are the colors that suit you:")
-
-    # Display colors
-    for color in colors:
-
-        st.markdown(
-            f"<div style='width: 50px; height: 50px; background-color: rgb{color}; display: inline-block; margin: 5px;'></div>",
-            unsafe_allow_html=True)
-
-
-# Main function
+def open_url(url):
+    webbrowser.open(url)
 def main():
     # Streamlit UI
-    st.title('Selfie Segmentation App')
+    st.title('ReStyle')
 
-    # Sidebar controls
-    model_selection = st.sidebar.selectbox('Model Selection', options=[0, 1])
-
-    # File uploader for image
-    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("Upload an image of yourself", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        # Convert the file to an OpenCV image
+        # Display the uploaded image (optional)
+        #st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+
+        # Step 2: Display a video with autoplay
+        video_file = "./animation.mp4"  # Replace with the path to your video file
+
+        # Check if the file exists
+        try:
+            with open(video_file, "rb") as video:
+                st.video(video.read())
+            
+            # Simulate the video playing duration (example: 7 seconds)
+        except:
+            time.sleep(7)
+
+        # Simulate the video playing duration (example: 7 seconds)
+        #time.sleep(7)
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
         image = cv2.imdecode(file_bytes, 1)
 
-        # Apply selfie segmentation
-        segmented_image = apply_selfie_segmentation(image, model_selection)
+        segmented_image = apply_selfie_segmentation(image, 0)
 
-        # Detect faces in the segmented image
         faces = detect_faces(segmented_image)
 
-        # Crop image to the first detected face
         cropped_image = crop_to_face(segmented_image.copy(), faces)
 
-        # Convert OpenCV images to PIL format for display
         pil_segmented_image = Image.fromarray(cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB))
         if cropped_image is not None:
             pil_cropped_image = Image.fromarray(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
 
-            # Detect colors in the cropped image, excluding gray colors
             colors = detect_colors(cropped_image, num_colors=20, detect_lip_color=True)
 
-            # Convert colors to HSL and extract hues, saturation, and lightness values
             hsl_colors = [rgb_to_hsl(r, g, b) for r, g, b in colors]
             hues = [h for h, s, l in hsl_colors]
             lightness_values = [l for h, s, l in hsl_colors]
             saturation_values = [s for h, s, l in hsl_colors]
-
-            # Determine temperature
+            
             temperature = determine_temperature(hues)
-
-            # Determine overall depth
             depth = determine_depth(lightness_values)
-
-            # Determine overall chroma
             chroma = determine_chroma(saturation_values)
-
-            # Determine the season
             season = map_to_season(hues, lightness_values, saturation_values)
-            
-            
 
-            # Display the original image, segmented image, cropped image, and color palette
-            st.subheader("Original Image")
-            st.image(image, caption="Original Image", use_column_width=True)
-
-            st.subheader("Segmented Image with Detected Face")
-            st.image(pil_segmented_image, caption="Segmented Image with Detected Face", use_column_width=True)
-
-            st.subheader("Cropped Image with Detected Face")
-            st.image(pil_cropped_image, caption="Cropped Image with Detected Face", use_column_width=True)
-
-            st.subheader("Color Palette")
-            palette_col = st.columns(len(colors))
-            for col, color in zip(palette_col, colors):
-                color_square = np.zeros((100, 100, 3), dtype=np.uint8)
-                color_square[:, :] = color
-                col.image(color_square, use_column_width=True)
-
-            st.subheader(f"Overall Temperature: {temperature}")
-            st.subheader(f"Overall Depth: {depth}")
-            st.subheader(f"Overall Chroma: {chroma}")
-            st.subheader(f"Season: {season}")
             recommended_colors_url, recommended_colors_palette = recommend_colors(season)
+            st.markdown(
+                f'<a href="{recommended_colors_url}" target="_blank" class="button">Check out the recommended products on Myntra</a>',
+                unsafe_allow_html=True
+            )
 
-            display_season_and_colors(season, recommended_colors_palette )
-
-            st.write(f"Check out the [recommended products on Myntra]({recommended_colors_url})")
-            
-
+            # Add custom CSS to style the link as a button
+            st.markdown(
+                """
+                <style>
+                .button {
+                    display: inline-block;
+                    padding: 10px 20px;
+                    font-size: 16px;
+                    font-weight: bold;
+                    color: white;
+                    background-color: #000000;
+                    border-radius: 5px;
+                    text-align: center;
+                    text-decoration: none;
+                }
+                .button:hover {
+                    background-color: #000000;
+                }
+                </style>
+                """,
+                unsafe_allow_html=True
+            )
 
 
 
